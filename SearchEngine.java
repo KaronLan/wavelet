5993c86014bc8fe61b44524d9b55261148455d44
KaronLan <100540201+KaronLan@users.noreply.github.com>
2022-10-20 10:13:22 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -2,33 +2,32 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
 
-class SearchEngineHandler implements URLHandler {
-    ArrayList <String> words = new ArrayList <String>();
+class Handler implements URLHandler {
+    ArrayList <String> items = new ArrayList <String>();
 
-    @Override
+   
     public String handleRequest(URI url) {
-
         if(url.getPath().equals("/")){
             return  "Let's start!";
-        }
-        else if (url.getPath().contains("/add")) {
+         }
+
+         else if (url.getPath().contains("/add")) {
             String[] parameters = url.getQuery().split("=");
-            if (parameters[0].equals("s")){
-                words.add(parameters[1]);
-            } 
-               return "Word added";
+           // if (parameters[0].equals("s")){
+                items.add(parameters[1]);
+           // } 
+               return "Word "+ parameters[1] +" added successfully";
         }
-        else {
+         else {
             System.out.println("Path: " + url.getPath());
 
             if (url.getPath().contains("/search")){
                 String[] search = url.getQuery().split("=");
                 if (search[0].equals("s")){
                     String searchReturn = ""; 
-                    for(String x: words){
+                    for(String x: items){
                         if(x.contains(search[1])){
-                            searchReturn += x;
-                            searchReturn += " and ";
+                            searchReturn += x + " ";
                         }
                     }
                     return searchReturn;
